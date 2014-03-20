a537add0928f93244d9808a56c346d0600364f1b
Didrik Wachtmeister <didrik.wachtmeister@gmail.com>
2014-03-20 10:16:03 +0100

    initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cloudmail::Application.config.secret_key_base = 'c0a7192e02a0902b1e047709f4a9c22523c715628e585d433129617d9fe062f2e4df846e24d51eb487c068de82e5dcfb8cecb3f885b2bbe075a114c822415cc6'
